2037400027f33bb4c1b52180dc3dfd14abeb2bb2
Amir Sezavar Keshavarz <1188298+amirsk@users.noreply.github.com>
2023-08-03 21:29:14 +0100

    Added CodeDeploy and S3

--- main.tf
+++ main.tf
@@ -144,10 +144,70 @@ resource "aws_instance" "app_server" {
   subnet_id              = aws_subnet.app_server.id
   vpc_security_group_ids = [aws_security_group.app_server.id]
 
+  iam_instance_profile = aws_iam_instance_profile.ec2_s3_access_profile.name
+
+  user_data = <<-EOF
+              #!/bin/bash
+              sudo yum update -y
+              sudo yum install -y ruby
+              sudo yum install -y wget
+              cd /home/ec2-user
+              wget https://aws-codedeploy-us-east-1.s3.us-east-1.amazonaws.com/latest/install
+              chmod +x ./install
+              sudo ./install auto
+              EOF
+
   tags = {
     Type = "Terraform"
     Name = "app_server"
   }
+}
+
+# Create an IAM role for EC2 instances with access to Amazon S3
+resource "aws_iam_role" "ec2_s3_access_role" {
+  name = "CodeDeployDemo-EC2-Instance-Profile"
+
+  assume_role_policy = jsonencode({
+    Version = "2012-10-17"
+    Statement = [
+      {
+        Action = "sts:AssumeRole"
+        Effect = "Allow"
+        Principal = {
+          Service = "ec2.amazonaws.com"
+        }
+      }
+    ]
+  })
+}
+
+# Attach a policy to the IAM role to grant S3 access
+resource "aws_iam_role_policy" "ec2_s3_access_policy" {
+  name = "CodeDeployDemo-EC2-Permissions"
+  role = aws_iam_role.ec2_s3_access_role.id
+
+  policy = jsonencode({
+    Version = "2012-10-17"
+    Statement = [
+      {
+        Action   = ["s3:Get*", "s3:List*"]
+        Effect   = "Allow"
+        Resource = "*"
+      }
+    ]
+  })
+}
+
+# Attach the AmazonSSMManagedInstanceCore policy to the IAM role
+resource "aws_iam_role_policy_attachment" "ec2_ssm_managed_policy_attachment" {
+  policy_arn = "arn:aws:iam::aws:policy/AmazonSSMManagedInstanceCore"
+  role       = aws_iam_role.ec2_s3_access_role.name
+}
+
+# Create an IAM instance profile and associate it with the IAM role
+resource "aws_iam_instance_profile" "ec2_s3_access_profile" {
+  name = "CodeDeployDemo-EC2-Instance-Profile"
+  role = aws_iam_role.ec2_s3_access_role.name
 }
 
 # Create an Elastic Block Store (EBS) volume
@@ -166,4 +226,71 @@ resource "aws_volume_attachment" "app_server" {
   device_name = "/dev/sdh"
   volume_id   = aws_ebs_volume.app_server.id
   instance_id = aws_instance.app_server.id
+}
+
+# Create an S3 Bucket for storing the application artifacts
+resource "aws_s3_bucket" "app_server" {
+  bucket = "aws-ec2-starter"
+
+  tags = {
+    Type = "Terraform"
+    Name = "app_server"
+  }
+}
+
+# Create an IAM service role for AWS CodeDeploy
+resource "aws_iam_role" "codedeploy_service_role" {
+  name = "code_deploy_service_role"
+
+  assume_role_policy = jsonencode({
+    Version = "2012-10-17"
+    Statement = [
+      {
+        Action = "sts:AssumeRole"
+        Effect = "Allow"
+        Principal = {
+          Service = "codedeploy.amazonaws.com"
+        }
+      }
+    ]
+  })
+}
+
+# Attach the AWSCodeDeployRole policy to the IAM role
+resource "aws_iam_role_policy_attachment" "codedeploy_service_role_attachment" {
+  policy_arn = "arn:aws:iam::aws:policy/service-role/AWSCodeDeployRole"
+  role       = aws_iam_role.codedeploy_service_role.name
+}
+
+# Create an AWS CodeDeploy application
+resource "aws_codedeploy_app" "aws-ec2-starter-app" {
+  name             = "aws-ec2-starter"
+  compute_platform = "Server"
+}
+
+# Create an AWS CodeDeploy deployment group
+resource "aws_codedeploy_deployment_group" "aws-ec2-starter-deployment_group" {
+  app_name              = aws_codedeploy_app.aws-ec2-starter-app.name
+  deployment_group_name = "AwsEc2StarterDeploymentGroup"
+
+  auto_rollback_configuration {
+    enabled = true
+    events  = ["DEPLOYMENT_FAILURE"]
+  }
+
+  ec2_tag_set {
+    ec2_tag_filter {
+      key   = "Type"
+      type  = "KEY_AND_VALUE"
+      value = "Terraform"
+    }
+  }
+
+  service_role_arn = aws_iam_role.codedeploy_service_role.arn
+
+  deployment_config_name = "CodeDeployDefault.OneAtATime"
+
+  # Specify the S3 bucket and object key where deployment artifacts are stored
+
+
 }
--- outputs.tf
+++ outputs.tf
@@ -1,33 +1,68 @@
-################################################################################
+#############################
+# VPC
+#############################
+output "vpc_id" {
+  value = aws_vpc.app_server.id
+}
+
+#############################
+# Internet Gateway
+#############################
+output "internet_gateway_id" {
+  value = aws_internet_gateway.app_server.id
+}
+
+#############################
+# Subnet
+#############################
+output "subnet_id" {
+  value = aws_subnet.app_server.id
+}
+
+#############################
+# Route Table
+#############################
+output "route_table_id" {
+  value = aws_route_table.app_server.id
+}
+
+#############################
+# Security Group
+#############################
+output "security_group_id" {
+  value = aws_security_group.app_server.id
+}
+
+#############################
 # EC2
-################################################################################
-output "id" {
+#############################
+output "aws_instance_id" {
   value = aws_instance.app_server.id
 }
 
-output "arn" {
+output "aws_instance_arn" {
   value = aws_instance.app_server.arn
 }
 
-output "private_dns" {
+output "aws_instance_private_dns" {
   value = aws_instance.app_server.private_dns
 }
 
-output "public_dns" {
+output "aws_instance_public_dns" {
   value = aws_instance.app_server.public_dns
 }
 
-output "private_ip" {
+output "aws_instance_private_ip" {
   value = aws_instance.app_server.private_ip
 }
 
-output "public_ip" {
+output "aws_instance_public_ip" {
   value = aws_instance.app_server.public_ip
 }
 
-################################################################################
-# Block Devices
-################################################################################
+#############################
+# EBS
+#############################
 output "ebs_volume_id" {
   description = "The ID of the EBS volume."
   value       = aws_ebs_volume.app_server.id
@@ -36,4 +71,22 @@ output "ebs_volume_id" {
 output "ebs_volume_size" {
   description = "The size of the EBS volume in GB."
   value       = aws_ebs_volume.app_server.size
+}
+
+#############################
+# S3
+#############################
+output "s3_bucket_name" {
+  value = aws_s3_bucket.app_server.bucket
+}
+
+#############################
+# CodeDeploy
+#############################
+output "code_deploy_app_name" {
+  value = aws_codedeploy_app.aws-ec2-starter-app.name
+}
+
+output "code_deploy_deployment_group_name" {
+  value = aws_codedeploy_deployment_group.aws-ec2-starter-deployment_group.deployment_group_name
 }
